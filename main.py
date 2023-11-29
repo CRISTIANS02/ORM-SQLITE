4407d712f1908cf3e936f8951d8a9496b504821b
cristians02 <cristiandelmar06@gmail.com>
2023-11-29 13:17:20 -0500

    practica sentencias de orm del profesin alvarez

--- main.py
+++ main.py
@@ -2,71 +2,70 @@ import orm
 from Tablas.Autores import Autores
 from Tablas.Libros import Libros
 from Tablas.Usuarios import Usuarios
-db=orm.SQLiteORM("db_biblioteca")
 
+db=orm.SQLiteORM("db_biblioteca")
 db.crear_tabla(Autores)
 db.crear_tabla(Libros)
 db.crear_tabla(Usuarios)
 
-autor_uno={
-    
-    "dni":78945612,
-    "nombre":"Quevedo",
-    "apellido":"Escoja de los rios"
-    }
-
-db.insertarUno("Autores",autor_uno)
-
+# autor_uno={
+#     "dni":78945612,
+#     "nombre":"quevedo",
+#     "apellidos":"es coja de los rios"
+#     }
+# db.insertarUno("Autores",autor_uno)
 usuarios_varios=[
     {
-        
-         "dni":95236895,
-         "nombres":"Emerson",
-         "apellidos":"Lopez",
-         "f_nacimiento":"02/10/2003",
-         "estado":"Activa"
-     },
-    {
-         "dni":95236895,
-         "nombres":"Juan",
-         "apellidos":"Lopez",
-         "f_nacimiento":"02/10/2003",
-         "estado":"Activa"
-    
+        "dni":745896,
+        "nombre":"nadine",
+        "apellidos":"atoccsa",
+        "f_nacimiento":"06/04/2005",
+        "estado":"activo"
     },
     {
-         "dni":95236895,
-         "nombres":"Juanito",
-         "apellidos":"Lopez",
-         "f_nacimiento":"02/10/2003",
-         "estado":"Activa"
+        "dni":78569784,
+        "nombre":"feli",
+        "apellidos":"ccaccachahua",
+        "f_nacimiento":"07/11/1997",
+        "estado":"activo"
     },
     {
-         "dni":952354895,
-         "nombres":"Rul",
-         "apellidos":"rojas",
-         "f_nacimiento":"06/09/2013",
-         "estado":"Activa"
+        "dni":71234567,
+        "nombre":"bichota",
+        "apellidos":"de taype",
+        "f_nacimiento":"28/11/2023",
+        "estado":"inactivo"
     },
-     {
-        "dni":95236893,
-        "nombres":"ELmer",
-        "apellidos":"ramos",
-        "f_nacimiento":"08/10/08",
-        "estado":"Activa"
-    }]
-#db.insertarVarios("usuarios",usuarios_varios)
-# muestra la lista de tuplas
-#print(db.mostrar("usuarios"))
-# muestra el objeto con sus campos y sus valores
-#print(db.mostrar("usuarios",type="objeto"))
-#tambien puedo filtrar informacion
-# print(db.mostrar("Usuarios",where="nombre LIKE'cha%'",type="objeto"))
-# print(db.mostrar("Usuarios",where="dni=952354895",type="objeto"))
+    {
+        "dni":755896,
+        "nombre":"chamo menor",
+        "apellidos":"no jodas",
+        "f_nacimiento":"30/11/2023",
+        "estado":"activo"
+    },
+    {
+        "dni":775896,
+        "nombre":"yadira",
+        "apellidos":"quiero mami",
+        "f_nacimiento":"500 a.c",
+        "estado":"momia"
+    }
+]
+# db.insertarVarios("Usuarios",usuarios_varios)
 
-db.actualizar("Usuarios",{"f_nacimiento":"11/08/2005"},where="dni=74123698")
+# muestra una lista de tuplas
+# print(db.mostrar("Usuarios"))
+# # # muestra un objeto con sus campos y sus valores
+# print(db.mostrar("Usuarios",type="objeto"))
+# tambien puedo filtrar informacion
+# print(db.mostrar("Usuarios",where="estado='momia'",type="objeto"))
+# print(db.mostrar("Usuarios",where="nombre LIKE 'cha%'",type="objeto"))
+# print(db.mostrar("Usuarios",where="dni =71234567",type="objeto"))
 
-db.eliminar("Usuarios",where"nombre='Juanito'")
-db.eliminar("Usuarios",where"dni=952354895")
-
+# db.actualizar("Usuarios",{"estado":"activo"},where="nombre='yadira'")
+# db.actualizar("Usuarios",{"f_nacimiento":"11/08/2005"},where="dni=775896")
+# dato_actualizar={"nombre":"chamos","apellidos":"ya es salida"}
+# db.actualizar("Usuarios",dato_actualizar,where="dni=755896")
+db.eliminar("Usuarios",where="nombre='bichota'")
+db.eliminar("Usuarios",where="dni=755896")
 print(db.mostrar("Usuarios",type="objeto"))
